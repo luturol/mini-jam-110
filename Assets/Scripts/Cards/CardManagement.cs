1e8ee7f1e50cdd4b0f48a6bc83a13cad751b75a6
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-07-08 23:03:59 -0300

    Fix cards collors and created event

--- Assets/Scripts/Cards/CardManagement.cs
+++ Assets/Scripts/Cards/CardManagement.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CardManagement : MonoBehaviour
 {
+    [Header("Debug Pourposes")]
+    [SerializeField] private List<Card> _cards;
+
+    [Header("Manager Configurations")]
+    [SerializeField]private GameObject _backLogContent;    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _cards = GetComponentsInChildren<Card>().ToList();
+
+        _cards.ForEach(e => e.transform.SetParent(_backLogContent.transform));
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
+    {        
+    }    
 }
