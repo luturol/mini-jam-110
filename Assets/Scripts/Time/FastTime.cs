8ce0cac0d3d03d38ffa6ee7afc66f9357ad2e5dd
Rafael Ahrons <rafael.ahrons@gmail.com>
2022-07-09 19:33:50 -0300

    Add due date and fix time string

--- Assets/Scripts/Time/FastTime.cs
+++ Assets/Scripts/Time/FastTime.cs
@@ -10,6 +10,7 @@ public class FastTime : MonoBehaviour
     [SerializeField] private float _timeMultiplier;
 
     private TextMeshProUGUI _text;
+    private int days = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,7 @@ public class FastTime : MonoBehaviour
         _passedTime += Time.deltaTime * _timeMultiplier;
 
         var time = TimeSpan.FromSeconds(_passedTime);
-        _text.text = $"{ time.Hours }:{ time.Minutes }:{ time.Seconds }";
+        
+        _text.text = $"{ time.Days.ToString("D2")}d { time.Hours.ToString("D2") }:{ time.Minutes.ToString("D2") }:{ time.Seconds.ToString("D2") }";
     }
 }
